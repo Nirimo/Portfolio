55517ba5db3a686fa47226f45d49c9411635d297
Nirimo <faustin.legrand1@gmail.com>
2024-03-03 01:33:26 +0100

    Repair the bugs of dosn't display image

--- components/robot/robot.tsx
+++ components/robot/robot.tsx
@@ -5,18 +5,19 @@ import robotGif from "./../../assets/robot.gif"
 const Robot: React.FC = () => {
   return (
         <div className="flex flex-row items-center justify-center mt-[200px]">
-            <div className="flex flex-row items-center justify-start gap-[600px] w-[80%]">
-                <div className="flex flex-col gap-10">
-                    <p className="text-white text-5xl">Salut,<br/> Je m'appel Faustin</p>
-                    <p className="text-[#a8a8a8] text-xl w-[450px]">Un développeur talentueux en full-stack avec <br />plus d'un an d'éxperience avec vous</p>
+            <div className="flex flex-row items-center justify-start gap-[600px] w-[80%] max-lg:gap-[0px]">
+                <div className="flex flex-col gap-10 w-max">
+                    <p className="text-white text-5xl ">Salut,<br/>Je m'appel Faustin</p>
+                    <p className="text-[#a8a8a8] text-xl">Un développeur talentueux en full-stack<br />avec plus d'un an d'éxperience avec vous</p>
                     <a href="" className="flex flex-row items-center justify-center text-white bg-blue-700 px-4 py-2 rounded-[10px] text-lg w-[150px]">Me Contacter</a>
                 </div>
-                <div>
+                <div className='max-lg:hidden'>
                     <Image 
                     src={robotGif}
                     alt="Gif of robot for presentation"
                     width={500}
-                    height={500} />
+                    height={500}
+                    />
                 </div>
             </div>
         </div>
--- components/skills/skills.tsx
+++ components/skills/skills.tsx
@@ -1,26 +1,34 @@
-"use client";
-
 import React from "react";
 import Image from "next/image";
 
+import skill from "./../../data/skills.json"
 
 
 const Skills: React.FC = () => {
   return (
-    <div className="mt-[270px]">
-      {skill.map((element, index) => (
-        <>
-          <Image
-            key={index}
-            src={`${basePath}${element.path}`}
-            alt={`Logo of ${element.name}`}
-            width={200}
-            height={200}
-          />
-          <p>{element.name}</p>
-          <p>{index}</p>
-        </>
-      ))}
+    <div className="mt-[200px]">
+      <div className="flex flex-col items-center justify-center gap-9">
+        <p id="skill" className="flex flex-row items-center justify-center text-center bg-black text-white w-[100px] rounded-md h-[40px]">Skills</p>
+        <p className="text-blue-700 text-5xl text-center">Toutes mes compétences</p>
+      </div>
+      <div className="flex flex-col items-center justify-center">
+        <div className="mt-[75px] w-[60%] flex flex-row flex-wrap gap-[90px] items-center justify-center max-sm:w-[90%] ">
+          {skill.map((element, index) => (
+            <div key={index} className="flex flex-col items-center justify-center gap-4 ">
+              <div className="w-[100px] h-[110px] flex flex-row items-center justify-center">
+                <Image
+                    src={`${element.path}`}
+                    alt={`Logo of ${element.name}`}
+                    width={100}
+                    height={100}
+                    className=""
+                  />
+              </div>  
+              <p key={index} className="text-white">{element.name}</p>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
