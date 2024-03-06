ec87bcc31a70282995bc06e03fa861d128bcfe2a
Nirimo <faustin.legrand1@gmail.com>
2024-03-06 23:23:33 +0100

    fix project name

--- components/project/project.tsx
+++ components/project/project.tsx
@@ -6,7 +6,7 @@ const Project: React.FC = () => {
   return (
         <div id="project" className="mt-[150px]">
             <div className="flex flex-col items-center justify-center gap-9">
-                <p id="skill" className="flex flex-row items-center justify-center text-center bg-black text-white w-[100px] rounded-md h-[40px]">Project</p>
+                <p id="skill" className="flex flex-row items-center justify-center text-center bg-black text-white w-[100px] rounded-md h-[40px]">Projets</p>
                 <p className="text-blue-700 text-5xl text-center">Projets</p>
                 <p className="text-[#a8a8a8] text-xl text-center">Vous trouverez ci-dessous les projets que j&apos;ai réalisé et dont je suis fier de les partager</p>
             </div>
